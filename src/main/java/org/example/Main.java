dfb9e9d7175bfd4df1db93d4eae622c7efda0e38
igprad <ign.aldi.pradana@gmail.com>
2025-03-07 15:06:43 +0700

    chore: add and apply spotless palantir java formatter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,14 @@
+/* igprad - (C) 2025 */
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableWebFlux
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
--- src/main/java/org/example/controller/EventController.java
+++ src/main/java/org/example/controller/EventController.java
@@ -1,4 +1,27 @@
+/* igprad - (C) 2025 */
 package org.example.controller;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
 public class EventController {
+
+    @GetMapping(path = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<String>> streamEvents() {
+        return Flux.interval(Duration.of(1, ChronoUnit.SECONDS)).map(this::construct);
+    }
+
+    private ServerSentEvent<String> construct(Long second) {
+        return ServerSentEvent.<String>builder()
+                .event("webflux-sse-event-test")
+                .data("Streamed event in " + second + " second.")
+                .comment("test")
+                .build();
+    }
 }
